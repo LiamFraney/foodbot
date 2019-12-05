11ed7fb6acaa3228a59e54bf627ac1ab8fdd2319
Liam Franey <32670606+LiamFraney@users.noreply.github.com>
2019-12-05 22:27:01 +0000

    Added GPU memory watcher

--- cnn/CNN.py
+++ cnn/CNN.py
@@ -4,6 +4,20 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
 class_names = []
 with open("food41/meta/meta/classes.txt") as reader:
     for class_name in reader:
