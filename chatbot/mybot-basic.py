9d22d23f8f2bb5d3018d1d175e4e738a9a45bede
Liam Franey <32670606+LiamFraney@users.noreply.github.com>
2019-12-05 22:20:41 +0000

    Initial commit of foodbot

---
+++ chatbot/mybot-basic.py
@@ -0,0 +1,97 @@
+﻿import spoonacular as sp
+import json, requests
+import aiml
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from xml.etree import ElementTree as ET
+kern = aiml.Kernel()
+kern.setTextEncoding(None)
+kern.bootstrap(learnFiles="C:/Users/LiamF/Documents/FinalYear/ArtificialIntelligence/Lab2/mybot-basic.xml")
+
+APIkey = "13ffaf56553e48faab0c6ce7762f2e1c" 
+spoon_api = sp.API(APIkey)
+
+print("Welcome to food bot")
+
+
+patterns = []
+tree = ET.parse("mybot-basic.xml")
+all_pattern = tree.findall("*/pattern")
+for pattern in all_pattern:
+    if "*" not in pattern.text:
+        patterns.append(pattern.text)
+
+def get_similar(phrase):
+    corpus = list(patterns)
+    corpus.append(phrase)
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(corpus)
+    similarity = list(cosine_similarity(tfidf_matrix, tfidf_matrix[-1]).flatten()[:-1])
+    if max(similarity) < 0.1:
+        print("Sorry I do not understand")
+    else:
+        similar_index = similarity.index(max(similarity))
+        # print(max(similarity))
+        # print(patterns[similar_index])
+        handle_answer(kern.respond(patterns[similar_index]))
+
+def handle_answer(answer):
+    if answer[0] == '#':
+        params = answer[1:].split('$')
+        cmd = int(params[0])
+        if cmd == 0:
+            print(params[1])
+            return True
+        elif cmd == 1:
+            response = spoon_api.search_recipes_complex(params[1])
+            data = response.json()
+            if data["results"]:
+                recipe_id = data["results"][0]["id"]
+                response = spoon_api.get_recipe_information(recipe_id)
+                data = response.json()
+                print(data["sourceUrl"])
+            else:
+                print("There is no recipe for this")
+
+        elif cmd == 2:
+            response = spoon_api.get_a_random_food_joke()
+            data = response.json()
+            print(data["text"])
+        
+        elif cmd == 3:
+            response = spoon_api.get_random_food_trivia()
+            data = response.json()
+            print(data["text"])
+        
+        elif cmd == 4:
+                q = params[1]
+                response = spoon_api.quick_answer("How much " + q)
+                data = response.json()
+                print(data["answer"])
+
+        elif cmd == 4:
+                q = params[1]
+                
+
+        elif cmd == 99:
+            get_similar(answer)
+
+    else:
+        print(answer)
+
+while True:
+    #get user input
+    try:
+        userInput = input("> ")
+    except (KeyboardInterrupt, EOFError) as e:
+        print("Bye!")
+        break
+    #pre-process user input and determine response agent (if needed)
+    responseAgent = 'aiml'
+    #activate selected response agent
+    if responseAgent == 'aiml':
+        answer = kern.respond(userInput)
+
+    if handle_answer(answer):
+        break
---
+++ cnn/CNN.py
@@ -0,0 +1,87 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+
+class_names = []
+with open("food41/meta/meta/classes.txt") as reader:
+    for class_name in reader:
+        class_names.append(class_name.strip())
+
+dataset_dir = "food41/images"
+model_filename = "model.h5"
+batch_size = 32
+num_epochs = 10
+img_height = 100
+img_width = 100
+color_bands = 3
+
+input_shape = (img_height, img_width, color_bands)
+
+datagen = ImageDataGenerator(rescale=1./255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            validation_split=0.2)
+
+train_generator = datagen.flow_from_directory(
+    directory=dataset_dir,
+    target_size=(img_height, img_width),
+    shuffle=True,
+    batch_size=batch_size,
+    class_mode="categorical",
+    subset="training")
+
+validation_generator = datagen.flow_from_directory(
+    directory=dataset_dir,
+    target_size=(img_height, img_width),
+    shuffle=True,
+    batch_size=batch_size,
+    class_mode="categorical",
+    subset="validation")
+
+# Create CNN
+model = Sequential([
+    Conv2D(filters=32, kernel_size=(3, 3), activation="relu", padding="same", input_shape=input_shape),
+    BatchNormalization(axis=-1),
+    MaxPooling2D((3, 3)),
+    Dropout(rate=0.25),
+    
+    Conv2D(filters=64, kernel_size=(3, 3), activation="relu", padding="same", input_shape=input_shape),
+    BatchNormalization(axis=-1),
+    Conv2D(filters=64, kernel_size=(3, 3), activation="relu", padding="same", input_shape=input_shape),
+    BatchNormalization(axis=-1),
+    MaxPooling2D((2, 2)),
+    Dropout(rate=0.25),
+
+    Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same", input_shape=input_shape),
+    BatchNormalization(axis=-1),
+    Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same", input_shape=input_shape),
+    BatchNormalization(axis=-1),
+    MaxPooling2D((2, 2)),
+    Dropout(rate=0.25),
+
+    Flatten(),
+    Dense(units=1024, activation="relu"),
+    BatchNormalization(),
+    Dropout(rate=0.5),
+    Dense(units=len(class_names), activation="softmax")
+])
+
+# Display model summary
+model.summary()
+
+# Compile model
+model.compile(optimizer="adam",
+            loss="categorical_crossentropy",
+            metrics=["accuracy"])
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch = train_generator.samples // batch_size,
+    validation_data = validation_generator, 
+    validation_steps = validation_generator.samples // batch_size,
+    epochs = num_epochs)
+
+model.save(model_filename)
